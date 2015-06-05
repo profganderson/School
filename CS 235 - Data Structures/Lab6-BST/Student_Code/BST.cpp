151a1891a08cc965093f33b7eb7bd44d5e1d9540
Andy Mockler <asmockler@gmail.com>
2015-06-05 14:30:37 -0600

    Begin Lab 6

---
+++ CS 235 - Data Structures/Lab6-BST/Student_Code/BST.cpp
@@ -0,0 +1,25 @@
+#include "BST.h"
+
+BST::BST() {
+
+}
+
+BST::~BST() {
+	clear();
+}
+
+NodeInterface * BST::getRootNode() {
+	return NULL;
+}
+
+bool BST::add(int data) {
+	return false;
+}
+
+bool BST::remove(int data) {
+	return false;
+}
+
+void BST::clear() {
+
+}
