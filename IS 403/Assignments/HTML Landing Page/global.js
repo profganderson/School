0173d1b9a9da2cae95a92dc1461ed1f4bb680fd5
asmockler <asmockler@gmail.com>
2015-09-21 10:30:23 -0600

    Finish Landing Page

--- IS 403/Assignments/HTML Landing Page/global.js
+++ IS 403/Assignments/HTML Landing Page/global.js
@@ -18,10 +18,16 @@ $(document).ready(function () {
 
 	$('.sidenav a').on('click', function (e) {
 		e.preventDefault();
+		var href = $(this).attr('href');
+
+		if (href === "#")
+			return;
+
 		var newPage = './' + $(this).attr('href');
 		$('body').fadeOut(300, function () {
 			window.location.href = newPage;
 		});
+
 	})
 });
 
@@ -29,29 +35,9 @@ $(document).ready(function () {
 // TODO
 
 // 1)  Home page (named index.html), should include: 
-//     a.    Navigation to the other pages in the site 
-//     b.    Your photo and contact information you are comfortable with making publicly available 
 //     c.    Either a personal statement, goal statement, or mission/vision statement. This is often included on a resume, but it will remain on the home page of this site.
 
-// 2)  Resume (named resume.html and in a folder called “resume”), should include: 
-//     a.    Navigation to the other pages in the site 
-//     b.    Only these five sections in this order: 
-//     		i.    Education 
-//     		ii.    Experience 
-//     		iii.    Skills and Certifications
-//     		iv    Awards 
-//     		v.    References
-//     				“Available upon request”—do not put anyone else's contact info on the web. 
-//     c.     Links within each section to the websites of your university, employers, cert., awards
-
 //  3) Personal interests (named interests.html or about.html), should include: 
-// 			a.    Navigation to the other pages in the site 
 // 			b.    Information about hobbies, non-professional achievements (e.g. hiked Mt. Everest, ran
 // 						marathon, etc.), and anything that might impress or interest potential employers. 
-// 			c.    Nothing that you want to keep private. It is okay to share personal interests, but don’t tell them where you live, how much money you have, etc.
  
-
-// Your final page should look something like the picture below and will require the following div’s:
-// Header, SideNav, and Footer.
-// Additionally, you will need to use the box model CSS styling to place elements appropriately in their respective div’s
-// The TA’s will dock points if a lack of effort in styling is apparent.
